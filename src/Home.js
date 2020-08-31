40524785164158898cc2e945176998543ec222f3
Cappuu <w8237228@icloud.com>
2020-08-31 22:25:32 +0800

    將原始 App.js 改名為 Home.js 刪除 creat-react-app 預設之圖檔

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './reset.css';
+import Header from './components/Hearder/Header'
+
+function Home() {
+  return (
+    <div className="home">
+      <Header />
+    </div>
+  );
+}
+
+export default Home;
