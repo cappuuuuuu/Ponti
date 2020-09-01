f73cddba7e52add26441e0c8301ac2061149ab66
Cappuu <w8237228@icloud.com>
2020-09-02 00:12:36 +0800

    click、hover  header 連結 有粗體字效果

--- src/components/Hearder/Header.js
+++ src/components/Hearder/Header.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React , { useEffect  ,useState } from 'react';
 import './Header.scss';
-import logo from '../../assets/ponti-wine-cellars-logo-1589532960.jpg.png'
+import logo from '../../assets/image/ponti-wine-cellars-logo-1589532960.jpg.png'
 import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 import SearchIcon from '@material-ui/icons/Search';
 
+
+
 function Header() {
+  const [ headerStatus , setHeaderStatus ]  = useState('')
+
+  useEffect(()=>{
+    window.addEventListener('scroll', handleScroll);
+  },[])
+
+  function handleScroll(){
+    if (window.scrollY === 0 ){
+      setHeaderStatus('')
+    } else {
+      setHeaderStatus('no-top')
+    }
+  }
+
   return (
-    <div className="header">
+    <div className={`header ${headerStatus}`}>
       <div className="contact">
         <p className="tel">
           <span>TEL </span>
@@ -22,11 +38,31 @@ function Header() {
           <img src={logo} alt=""/>
         </div>
         <ul className="nav">
-          <li>SHOP</li>
-          <li>PROMOTIONS</li>
+          <li data-text="SHOP">
+            <input type="radio" name="navigation" id="shop"/> 
+            <label for="shop">SHOP</label>
+          </li>
+          <li data-text="PROMOTIONS">
+            <input type="radio" name="navigation" id="promotions"/> 
+            <label for="promotions">PROMOTIONS</label>
+          </li>
+          <li data-text="EVENTS">
+            <input type="radio" name="navigation" id="events"/> 
+            <label for="events">EVENTS</label>
+          </li>
+          <li data-text="ABOUTS">
+            <input type="radio" name="navigation" id="abouts"/> 
+            <label for="abouts">ABOUTS</label>
+          </li>
+          <li data-text="CONTACT">
+            <input type="radio" name="navigation" id="contact"/> 
+            <label for="contact">CONTACT</label>
+          </li>
+          {/* <li><a href="#promotions">PROMOTIONS</a>
+          </li>
           <li>EVENTS</li>
           <li>ABOUTS</li>
-          <li>CONTACT</li>
+          <li>CONTACT</li> */}
         </ul>
       </div>
       <div className="loginbar">
