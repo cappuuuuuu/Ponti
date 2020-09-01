e68dc3de7e69957ff08115495d59315da5d7c1ac
Cappuu <w8237228@icloud.com>
2020-09-02 00:17:48 +0800

    於桌機螢幕下限制 Widget 、Product 組件 最大寬度為 1450px

--- src/Home.js
+++ src/Home.js
@@ -4,13 +4,17 @@ import Header from './components/Hearder/Header'
 import Slider from './components/Slider/Slider'
 import Widget from './components/Widget/Widget'
 import Footer from './components/Footer/Footer'
+import Product from './components/Product/Product'
 
 function Home() {
   return (
     <div className="home">
       <Header />
       <Slider />
-      <Widget />
+      <div className="restrict-width-container">
+        <Widget />
+        <Product />
+      </div>
       <Footer />
     </div>
   );
