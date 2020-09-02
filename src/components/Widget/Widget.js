f366854dd91a9dbc7976cada350d3e0fefe10eb6
Cappuu <w8237228@icloud.com>
2020-09-02 15:42:56 +0800

    Widget 組件 RWD ， 圖片改以 background-image 引入

--- src/components/Widget/Widget.js
+++ src/components/Widget/Widget.js
@@ -29,28 +29,24 @@ function Widget() {
       <div className="border-line"></div>
       <ul className="widget-square">
         <li className="widget-square-item">
-          <img src={require('../../assets/image/heightlight-desktop-1.png')} alt=""/>
           <div className="info">
             <h5>Shop Promotions</h5>
             <p>Check out the latest offers in shops!</p>
           </div>
         </li>
         <li className="widget-square-item">
-          <img src={require('../../assets/image/heightlight-desktop-2.png')} alt=""/>
           <div className="info">
             <h5>Smart Buys</h5>
             <p>Well-priced, high-quality wines!</p>
           </div>
         </li>
         <li className="widget-square-item">
-          <img src={require('../../assets/image/heightlight-desktop-3.png')} alt=""/>
           <div className="info">
             <h5>Top Rated Wines</h5>
             <p>Wine scores 90 points or higher!</p>
           </div>
         </li>
         <li className="widget-square-item">
-          <img src={require('../../assets/image/heightlight-desktop-4.png')} alt=""/>
           <div className="info">
             <h5>Events</h5>
             <p>What is happening now!</p>
