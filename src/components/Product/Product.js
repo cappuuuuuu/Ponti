0e7432f75c148cfe07f94c38e1bf67e1abeda759
Cappuu <w8237228@icloud.com>
2020-09-03 01:47:43 +0800

    no message

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -79,41 +79,43 @@ function ProductList ({ category }) {
                     </div>
                     <div className="stock-status">{ product.stock ? '' : 'OUT OF STOCK' }</div>
                     <div className="product-price">HK${ product.price }</div>
-                </div>
-                <div className="product-img">
-                  <img src={require(`../../assets/image/${ product.imgUrl }`)} alt=""/>
-                </div>
-                <div className="product-details">
-                  <div className="details">
-                    <div className="vintage">
-                    Vintage : <span>{ product.vintage }</span>
-                    </div>
-                    <div className="country">
-                      country : <span>{ product.country }</span>
-                    </div>
-                    <div className="region">
-                      &nbsp; region : <span>{ product.region }</span>
-                    </div>
-                    <div className="size">
-                      &nbsp; &nbsp; &nbsp;  Size : <span>{ product.size }</span>
-                    </div>
+
                   </div>
-                  <span className="line horizontal top"></span>
-                  <span className="line horizontal bottom"></span>
-                  <span className="line vertical left"></span>
-                  <span className="line vertical right"></span>
+                  <div className="product-img">
+                    <img src={require(`../../assets/image/${ product.imgUrl }`)} alt=""/>
+                  </div>
+                
+                  <div className="product-details">
+                    <div className="details">
+                      <div className="vintage">
+                      Vintage : <span>{ product.vintage }</span>
+                      </div>
+                      <div className="country">
+                        country : <span>{ product.country }</span>
+                      </div>
+                      <div className="region">
+                        &nbsp; region : <span>{ product.region }</span>
+                      </div>
+                      <div className="size">
+                        &nbsp; &nbsp; &nbsp;  Size : <span>{ product.size }</span>
+                      </div>
+                    </div>
+                    <span className="line horizontal top"></span>
+                    <span className="line horizontal bottom"></span>
+                    <span className="line vertical left"></span>
+                    <span className="line vertical right"></span>
+                  </div>
+                  <div className="product-like">
+                    <FavoriteIcon className="product-like-icon"/>
+                  </div>
                 </div>
-                <div className="product-like">
-                  <FavoriteIcon className="product-like-icon"/>
-                </div>
-              </div>
-              <div className={`product-cart ${ product.stock ? '' : 'disabled'}`} >
-                <div className="input-cart-wrapper">
-                  <button className="decrease">-</button>
-                  <input type="text" className="input-cart" placeholder={product.stock?'1':'0'}/>
-                  <button className="increase">+</button>
-                </div>
-                <div>
+                <div className={`product-cart ${ product.stock ? '' : 'disabled'}`} >
+                  <div className="input-cart-wrapper">
+                    <button className="decrease">-</button>
+                    <input type="text" className="input-cart" placeholder={product.stock?'1':'0'}/>
+                    <button className="increase">+</button>
+                  </div>
+                  <div>
                   <button className="add-cart-btn" disabled={ !product.stock }>Add to cart</button>
                 </div>
               </div>
@@ -125,8 +127,6 @@ function ProductList ({ category }) {
     </Swiper>
   )
 }
-
-
 
 function Product () {
   return (
