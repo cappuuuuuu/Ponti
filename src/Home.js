3ef775e9dc94b24f749694336a430e802137fcd6
Cappuu <w8237228@icloud.com>
2020-09-03 14:14:52 +0800

    移除 reset.css

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './reset.css';
 import Header from './components/Hearder/Header'
 import Slider from './components/Slider/Slider'
 import Widget from './components/Widget/Widget'
